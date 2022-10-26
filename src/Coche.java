2289370fc3841f7b931a97c3736f0be8ce4235be
Hroobles <hernanrobles09@gmail.com>
2022-10-26 11:53:29 +0200

    Constructor coches

--- src/Coche.java
+++ src/Coche.java
@@ -3,4 +3,10 @@ public class Coche {
 	private String matricula;
 	private int cv;
 	private int puertas;
+	public Coche(String matricula, int cv, int puertas) {
+		super();
+		this.matricula = matricula;
+		this.cv = cv;
+		this.puertas = puertas;
+	}
 }
