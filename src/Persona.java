ec7926995ac87d0c24a788e61db743f342793ace
Hroobles <hernanrobles09@gmail.com>
2022-10-26 11:55:44 +0200

    Constructor personas

--- src/Persona.java
+++ src/Persona.java
@@ -3,4 +3,11 @@ public class Persona {
 	private String nombre;
 	private String apellido;
 	private int edad;
+	public Persona(String nombre, String apellido, int edad) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+	}
+	
 }
